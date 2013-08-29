fd35b6dd38731b5b908c8ec8e7a13980ea60b02a
Wolfgang Bumiller <wry.git@bumiller.com>
2013-08-29 15:47:31 +0200

    fixup repo permissions

--- install-server.sh
+++ install-server.sh
@@ -80,6 +80,7 @@ setup_admin_repo() {
     || die "failed to change directory to ${repo_home}/admin"
   [ -d "admin.git" ] || git init --bare admin.git \
     || die "failed to initialize admin git repository"
+  chown -R "${repo_uid}:${repo_gid}" admin.git
   submsg "setting up git push hook"
   ln -svf "${repo_home}/bin/admin-push-hook" "admin.git/hooks/post-receive" \
     || die "failed to setup post-receive git hook"
