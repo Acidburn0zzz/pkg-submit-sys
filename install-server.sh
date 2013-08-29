76079d8d3788cb5379c62acbd0338f14fa91e414
Wolfgang Bumiller <wry.git@bumiller.com>
2013-08-29 15:16:14 +0200

    pushd/popd so config_home still works

--- install-server.sh
+++ install-server.sh
@@ -76,13 +76,14 @@ copy_bin() {
 
 setup_admin_repo() {
   msg "setting up admin repository"
-  cd "${repo_home}/admin" \
+  pushd "${repo_home}/admin" \
     || die "failed to change directory to ${repo_home}/admin"
   [ -d "admin.git" ] || git init --bare admin.git \
     || die "failed to initialize admin git repository"
   submsg "setting up git push hook"
   ln -svf "${repo_home}/bin/admin-push-hook" "admin.git/hooks/post-receive" \
     || die "failed to setup post-receive git hook"
+  popd
 }
 
 config_home() {
