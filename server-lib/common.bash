5b73d9e617bc407551eed22bc56515759369059e
Wolfgang Bumiller <wry.git@bumiller.com>
2014-05-10 18:56:42 +0200

    new group based administration system

--- server-lib/common.bash
+++ server-lib/common.bash
@@ -7,9 +7,9 @@ err() {
 
 wrn() {
   local mesg="$1"; shift
-  printf " (remote) \033[1;33m---\033[0;0m ${mesg}\n" "$@"
+  printf " (remote) \033[1;33m---\033[0;0m ${mesg}\n" "$@" >&2
   printf "\033[1;33m---:\033[0;0m ${mesg}\n" "$@" \
-    >> "${path}/warnings"
+    2>/dev/null >> "${path}/warnings"
 }
 
 msg() {
