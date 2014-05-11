06396b6a6be357e7cca1c4ec46a65c0cd85e92f2
Wolfgang Bumiller <wry.git@bumiller.com>
2014-05-11 12:28:13 +0200

    execute removal commands; be verbose about the 'rm'

--- server-lib/commands.bash
+++ server-lib/commands.bash
@@ -49,8 +49,8 @@ remove_package_from() {
   fi
 
   log "removing package %s from %s/%s" "$pkg" "$repo" "$arch"
-  if echo repo-remove "${repo}.db.tar.gz" "${pkg}"; then
-    echo rm "${pkg}"-[0-9]*.pkg.tar.xz{,.sig}
+  if repo-remove "${repo}.db.tar.gz" "${pkg}"; then
+    rm -v "${pkg}"-[0-9]*.pkg.tar.xz{,.sig}
     run_hook remove_package "$pkg" "$repo" "$arch"
   else
     wrn "failed to remove package %s from %s/%s" "$pkg" "$repo" "$arch"
